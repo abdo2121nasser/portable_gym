ae07a26229005251c3cf6eb6601fb581f5e8c0c5
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2024-06-06 13:28:40 +0300

    font manager is initialized with family and size font

---
+++ portable_gym/lib/presentation/font_manager.dart
@@ -0,0 +1,20 @@
+class FontFamily
+{
+  static const String kPoppinsFont= 'Poppins';
+  //titles and subtitles
+  static const String kLeagueSpartanFont= 'League Spartan';
+  //body text
+}
+
+
+
+class FontSize
+{
+  static const double kSize12=12.0;
+  static const double kSize14=14.0;
+  static const double kSize16=16.0;
+  static const double kSize18=18.0;
+  static const double kSize20=20.0;
+  static const double kSize22=22.0;
+
+}
