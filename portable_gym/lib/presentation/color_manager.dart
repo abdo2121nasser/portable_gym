dfd1ac8985209993ea807493efd628fd5567e258
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2024-06-06 12:41:27 +0300

    color manager is initialized with colors

---
+++ portable_gym/lib/presentation/color_manager.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class ColorManager
+{
+  static const Color kWhiteColor = Color(0xffFFFFFF);
+  static const Color kBlackColor = Color(0xff232323);
+  static const Color kLimeGreenColor = Color(0xffE2F163);
+  static const Color kPurpleColor = Color(0xff896CFE);
+  static const Color kLightPurpleColor = Color(0xffB3A0FF);
+
+}
+
+
+
