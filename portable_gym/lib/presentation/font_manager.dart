315f02de8b6f372358d3f4a6ebbcf6be06d5ab41
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2024-06-06 14:23:25 +0300

    adding font weight to font manager file

--- portable_gym/lib/presentation/font_manager.dart
+++ portable_gym/lib/presentation/font_manager.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FontFamily
@@ -8,7 +10,15 @@ class FontFamily
   //body text
 }
 
-
+class FontWeightManager
+{
+  static const FontWeight bold =FontWeight.w700;
+  static const FontWeight light =FontWeight.w300;
+  static const FontWeight regular =FontWeight.w400;
+  static const FontWeight semiBold =FontWeight.w600;
+  static const FontWeight thin =FontWeight.w100;
+  static const FontWeight medium =FontWeight.w500;
+}
 
 class FontSize
 {
