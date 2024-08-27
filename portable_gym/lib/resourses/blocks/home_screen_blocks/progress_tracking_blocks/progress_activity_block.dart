378ab04617313c8277bbaf83922831b0db27aa3b
abdo2121nasser <109314430+abdo2121nasser@users.noreply.github.com>
2024-08-28 01:19:32 +0300

    filling the attributes in progress_activity_block.dart

--- portable_gym/lib/resourses/blocks/home_screen_blocks/progress_tracking_blocks/progress_activity_block.dart
+++ portable_gym/lib/resourses/blocks/home_screen_blocks/progress_tracking_blocks/progress_activity_block.dart
@@ -6,6 +6,7 @@ import 'package:portable_gym/resourses/models/progress_models/activity_model.dar
 import '../../../../generated/l10n.dart';
 import '../../../managers_files/font_manager.dart';
 import '../../../managers_files/style_manager.dart';
+import 'package:intl/intl.dart';
 
 
 class ProgressActivityBlock extends StatelessWidget {
@@ -29,14 +30,14 @@ class ProgressActivityBlock extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'upper body work out',
+                  activity.getCategoryName(context),
                   style: getMediumStyle(
                       fontSize: FontSize.s14,
                       color: ColorManager.kBlackColor,
                       fontFamily: FontFamily.kPoppinsFont),
                 ),
                 Text(
-                  'june 09',
+                  DateFormat('MMMM dd').format(activity.date),
                   style: getMediumStyle(
                       fontSize: FontSize.s14,
                       color: ColorManager.kLightPurpleColor,
@@ -51,11 +52,12 @@ class ProgressActivityBlock extends StatelessWidget {
                 const Icon(Icons.access_time_filled_outlined,color: ColorManager.kPurpleColor,),
               SizedBox(width: AppHorizontalSize.s5,),
                 Text(
-                  'Duration\n25 min',
+                  'Duration\n${DateFormat('HH:mm:ss').format(activity.period)}',
                   style: getMediumStyle(
                       fontSize: FontSize.s14,
                       color: ColorManager.kPurpleColor,
                       fontFamily: FontFamily.kPoppinsFont),
+                  textAlign: TextAlign.center,
                 ),
 
               ],
